1c36f034ee112c0fc3387e5e47dd25d239391578
CSJin <hamony1029@naver.com>
2023-08-29 17:09:26 +0900

    목적지 설정 버튼 연결 완료

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+from functools import partial
+
 from manualDriving import move_straight, turn_left, move_back, turn_right, stop_run
 from selDestination import assign_des, start_driving
 from PyQt5.QtWidgets import *
@@ -26,17 +28,37 @@ class WindowClass(QMainWindow):
         # 현재 페이지 인덱스 기록
         self.current_page_index = 0
 
+        #버튼 변수 정의
+        self.btn_selDes = self.ui_mainPage.btn_selDes            # 메인페이지 - 목적지설정
+        self.btn_manDriving = self.ui_mainPage.btn_manDriving    # 메인페이지 - 수동주행
+        self.btn_emrCall = self.ui_mainPage.btn_emrCall          # 메인페이지 - 긴급호출
+        self.btn1_1f = self.ui_selDestination.btn1_1f    # 목적지설정 - 1층-1
+        self.btn2_1f = self.ui_selDestination.btn2_1f    # 목적지설정 - 1층-2
+        self.btn3_1f = self.ui_selDestination.btn3_1f    # 목적지설정 - 1층-3
+        self.btn4_1f = self.ui_selDestination.btn4_1f    # 목적지설정 - 1층-4
+        self.btn5_1f = self.ui_selDestination.btn5_1f    # 목적지설정 - 1층-5
+        self.btn6_1f = self.ui_selDestination.btn6_1f    # 목적지설정 - 1층-6
+        self.btn7_1f = self.ui_selDestination.btn7_1f    # 목적지설정 - 1층-7
+        self.btn8_1f = self.ui_selDestination.btn8_1f    # 목적지설정 - 1층-8
+        self.btn_start = self.ui_selDestination.btn_start    # 목적지설정 - 주행시작
         # 버튼에 페이지 연결
-        self.ui_mainPage.btn_selDes.clicked.connect(self.show_selDestination)
-        self.ui_mainPage.btn_manDriving.clicked.connect(self.show_manualDriving)
-        self.ui_mainPage.btn_emrCall.clicked.connect(self.show_emrCall)
+        self.btn_selDes.clicked.connect(self.show_selDestination)
+        self.btn_manDriving.clicked.connect(self.show_manualDriving)
+        self.btn_emrCall.clicked.connect(self.show_emrCall)
         # 홈 버튼 연결
         self.ui_selDestination.btn_home.clicked.connect(self.show_mainPage)
         self.ui_manualDriving.btn_home.clicked.connect(self.show_mainPage)
         self.ui_emrCall.btn_home.clicked.connect(self.show_mainPage)
         # 목적지 설정
-        self.ui_selDestination.btn1_1f.clicked.connect(self.call_assign_des)
-        self.ui_selDestination.btn_start.clicked.connect(start_driving)
+        self.btn1_1f.clicked.connect(lambda: self.call_assign_des(self.btn1_1f))
+        self.btn2_1f.clicked.connect(partial(self.call_assign_des, btn=self.btn2_1f))
+        self.btn3_1f.clicked.connect(partial(self.call_assign_des, btn=self.btn3_1f))
+        self.btn4_1f.clicked.connect(partial(self.call_assign_des, btn=self.btn4_1f))
+        self.btn5_1f.clicked.connect(partial(self.call_assign_des, btn=self.btn5_1f))
+        self.btn6_1f.clicked.connect(partial(self.call_assign_des, btn=self.btn6_1f))
+        self.btn7_1f.clicked.connect(partial(self.call_assign_des, btn=self.btn7_1f))
+        self.btn8_1f.clicked.connect(partial(self.call_assign_des, btn=self.btn8_1f))
+        self.btn_start.clicked.connect(lambda: self.call_start_driving(self.btn_start))
         # 긴급 호출 버튼 연결
         self.ui_selDestination.btn_emrCall.clicked.connect(self.show_emrCall)
         self.ui_manualDriving.btn_emrCall.clicked.connect(self.show_emrCall)
@@ -63,20 +85,24 @@ class WindowClass(QMainWindow):
         self.current_page_index = 1
 
     def show_manualDriving(self):
-        # selDestination 페이지로 전환
+        # manualDriving 페이지로 전환
         print("수동주행")
         self.stacked_widget.setCurrentIndex(2)
         self.current_page_index = 2
 
     def show_emrCall(self):
-        # selDestination 페이지로 전환
+        # emrCall 페이지로 전환
         print("긴급호출")
         self.stacked_widget.setCurrentIndex(3)
         self.current_page_index = 3
 
-    def call_assign_des(self):
-        print(self.ui_selDestination.btn1_1f.text())
-        assign_des(self.ui_selDestination.btn1_1f)
+    def call_assign_des(self,btn):
+        print("call_assign_des")
+        assign_des(btn)
+
+    def call_start_driving(self, btn):
+        print("call_start_driving")
+        start_driving(btn)
 
 
 if __name__ == "__main__":
--- selDestination.py
+++ selDestination.py
@@ -3,16 +3,44 @@ des_x, des_y = 0, 0
 
 def assign_des(btn):
     print(btn.text())
+    global des_x, des_y # 외부에서 전역 변수를 사용하기 위해 global 선언
+
     if btn.text() == "CT촬영실":
-        global des_x, des_y # 외부에서 전역 변수를 사용하기 위해 global 선언
+
         des_x = 1  # db에서 조회한 좌표를 변수에 할당
         des_y = 1
+    elif btn.text() == "비뇨기과":
+        des_x = 1
+        des_y = 2
+    elif btn.text() == "비상계단":
+        des_x = 1
+        des_y = 3
+    elif btn.text() == "이비인후과":
+        des_x = 2
+        des_y = 1
+    elif btn.text() == "접수처":
+        des_x = 2
+        des_y = 2
+    elif btn.text() == "치과":
+        des_x = 2
+        des_y = 3
+    elif btn.text() == "화장실":
+        des_x = 3
+        des_y = 1
+    elif btn.text() == "핵의학과":
+        des_x = 3
+        des_y = 2
     print(des_x, des_y)
 
 
-def start_driving(self):
+def start_driving(btn):
+    global des_x, des_y
     print(des_x, des_y)
-
+    # 클릭 시 버튼 텍스트 전환
+    if btn.text() == "주행시작":
+        btn.setText("정지")
+    elif btn.text() == "정지":
+        btn.setText("주행시작")
     # 지정좌표로 이동할 수 있는 py파일에 연결
 
 
