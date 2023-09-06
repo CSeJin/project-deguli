1f8e70949d393147dfdf10360e8f8adef3f6d991
eunhye <eunhyee657@gmail.com>
2023-09-06 17:34:29 +0900

    전용 레파지토리 생성 후 파일 이동

---
+++ main.py
@@ -0,0 +1,198 @@
+import sys
+import time
+from functools import partial
+
+from PyQt5.uic.properties import QtCore
+
+import emrCall
+import manualDriving
+import manualDriving_ui, mainPage_ui, emrCall_ui, selDestination_ui
+from selDestination import assign_des, start_driving
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QTimer
+
+
+# 화면을 띄우는데 사용되는 Class 선언
+class WindowClass(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # UI 페이지 전환을 위한 QStackedWidget 생성
+        self.stacked_widget = QStackedWidget(self)
+        self.stacked_widget.addWidget(mainPage_ui.Ui_MainPage())
+        self.stacked_widget.addWidget(selDestination_ui.Ui_selDestination())
+        self.stacked_widget.addWidget(manualDriving_ui.Ui_ManualDriving())
+        self.stacked_widget.addWidget(emrCall_ui.Ui_EmrCall())
+        # 현재 페이지 인덱스 기록
+        self.current_page_index = 0
+        # 초기 화면 설정
+        self.setCentralWidget(self.stacked_widget)
+
+        #############################################################
+        # 버튼 변수 정의 -------------------------------------------------
+        # mainPage_ui ------
+        self.btn_selDes = mainPage_ui.Ui_MainPage.btn_selDes  # 메인페이지 - 목적지설정
+        self.btn_manDriving = mainPage_ui.Ui_MainPage.btn_manDriving  # 메인페이지 - 수동주행
+        self.btn_emrCall = mainPage_ui.Ui_MainPage.btn_emrCall  # 메인페이지 - 긴급호출
+        # selDestination_ui ------
+        self.btn_home_selDestination = selDestination_ui.Ui_selDestination.btn_home
+        # 1층
+        self.btn1_1f = selDestination_ui.Ui_selDestination.btn1_1f  # 목적지설정 - 1층-1
+        self.btn2_1f = selDestination_ui.Ui_selDestination.btn2_1f  # 목적지설정 - 1층-2
+        self.btn3_1f = selDestination_ui.Ui_selDestination.btn3_1f  # 목적지설정 - 1층-3
+        self.btn4_1f = selDestination_ui.Ui_selDestination.btn4_1f  # 목적지설정 - 1층-4
+        self.btn5_1f = selDestination_ui.Ui_selDestination.btn5_1f  # 목적지설정 - 1층-5
+        self.btn6_1f = selDestination_ui.Ui_selDestination.btn6_1f  # 목적지설정 - 1층-6
+        self.btn7_1f = selDestination_ui.Ui_selDestination.btn7_1f  # 목적지설정 - 1층-7
+        self.btn8_1f = selDestination_ui.Ui_selDestination.btn8_1f  # 목적지설정 - 1층-8
+        # # 2층
+        # self.btn1_2f = selDestination_ui.Ui_selDestination.btn1_2f    # 목적지설정 - 1층-1
+        # self.btn2_2f = selDestination_ui.Ui_selDestination.btn2_2f    # 목적지설정 - 1층-2
+        # self.btn3_2f = selDestination_ui.Ui_selDestination.btn3_2f    # 목적지설정 - 1층-3
+        # self.btn4_2f = selDestination_ui.Ui_selDestination.btn4_2f    # 목적지설정 - 1층-4
+        # self.btn5_2f = selDestination_ui.Ui_selDestination.btn5_2f    # 목적지설정 - 1층-5
+        # self.btn6_2f = selDestination_ui.Ui_selDestination.btn6_2f    # 목적지설정 - 1층-6
+        # self.btn7_2f = selDestination_ui.Ui_selDestination.btn7_2f    # 목적지설정 - 1층-7
+        # self.btn8_2f = selDestination_ui.Ui_selDestination.btn8_2f    # 목적지설정 - 1층-8
+        # #3층
+        # self.btn1_3f = selDestination_ui.Ui_selDestination.btn1_3f    # 목적지설정 - 1층-1
+        # self.btn2_3f = selDestination_ui.Ui_selDestination.btn2_3f    # 목적지설정 - 1층-2
+        # self.btn3_3f = selDestination_ui.Ui_selDestination.btn3_3f    # 목적지설정 - 1층-3
+        # self.btn4_3f = selDestination_ui.Ui_selDestination.btn4_3f    # 목적지설정 - 1층-4
+        # self.btn5_3f = selDestination_ui.Ui_selDestination.btn5_3f    # 목적지설정 - 1층-5
+        # self.btn6_3f = selDestination_ui.Ui_selDestination.btn6_3f    # 목적지설정 - 1층-6
+        # self.btn7_3f = selDestination_ui.Ui_selDestination.btn7_3f    # 목적지설정 - 1층-7
+        # self.btn8_3f = selDestination_ui.Ui_selDestination.btn8_3f    # 목적지설정 - 1층-8
+        # #4층
+        # self.btn1_4f = selDestination_ui.Ui_selDestination.btn1_4f    # 목적지설정 - 1층-1
+        # self.btn2_4f = selDestination_ui.Ui_selDestination.btn2_4f    # 목적지설정 - 1층-2
+        # self.btn3_4f = selDestination_ui.Ui_selDestination.btn3_4f    # 목적지설정 - 1층-3
+        # self.btn4_4f = selDestination_ui.Ui_selDestination.btn4_4f    # 목적지설정 - 1층-4
+        # self.btn5_4f = selDestination_ui.Ui_selDestination.btn5_4f    # 목적지설정 - 1층-5
+        # self.btn6_4f = selDestination_ui.Ui_selDestination.btn6_4f    # 목적지설정 - 1층-6
+        # self.btn7_4f = selDestination_ui.Ui_selDestination.btn7_4f    # 목적지설정 - 1층-7
+        # self.btn8_4f = selDestination_ui.Ui_selDestination.btn8_4f    # 목적지설정 - 1층-8
+        # #5층
+        # self.btn1_5f = selDestination_ui.Ui_selDestination.btn1_5f    # 목적지설정 - 1층-1
+        # self.btn2_5f = selDestination_ui.Ui_selDestination.btn2_5f    # 목적지설정 - 1층-2
+        # self.btn3_5f = selDestination_ui.Ui_selDestination.btn3_5f    # 목적지설정 - 1층-3
+        # self.btn4_5f = selDestination_ui.Ui_selDestination.btn4_5f    # 목적지설정 - 1층-4
+        # self.btn5_5f = selDestination_ui.Ui_selDestination.btn5_5f    # 목적지설정 - 1층-5
+        # self.btn6_5f = selDestination_ui.Ui_selDestination.btn6_5f    # 목적지설정 - 1층-6
+        # self.btn7_5f = selDestination_ui.Ui_selDestination.btn7_5f    # 목적지설정 - 1층-7
+        # self.btn8_5f = selDestination_ui.Ui_selDestination.btn8_5f    # 목적지설정 - 1층-8
+        # #6층
+        # self.btn1_6f = selDestination_ui.Ui_selDestination.btn1_6f    # 목적지설정 - 1층-1
+        # self.btn2_6f = selDestination_ui.Ui_selDestination.btn2_6f    # 목적지설정 - 1층-2
+        # self.btn3_6f = selDestination_ui.Ui_selDestination.btn3_6f    # 목적지설정 - 1층-3
+        # self.btn4_6f = selDestination_ui.Ui_selDestination.btn4_6f    # 목적지설정 - 1층-4
+        # self.btn5_6f = selDestination_ui.Ui_selDestination.btn5_6f    # 목적지설정 - 1층-5
+        # self.btn6_6f = selDestination_ui.Ui_selDestination.btn6_6f    # 목적지설정 - 1층-6
+        # self.btn7_6f = selDestination_ui.Ui_selDestination.btn7_6f    # 목적지설정 - 1층-7
+        # self.btn8_6f = selDestination_ui.Ui_selDestination.btn8_6f    # 목적지설정 - 1층-8
+        self.btn_start = selDestination_ui.Ui_selDestination.btn_start
+        self.btn_emrCall_selDestination = selDestination_ui.Ui_selDestination.btn_emrCall
+
+        # 버튼 변수를 문자열 형태로 리스트에 추가
+        self.btn_list = []
+        for i in range(1, 9):
+            var_name = f"btn{i}_1f"
+            self.btn_list.append(var_name)
+        # manualDriving_ui ------
+        self.btn_home_manualDriving = manualDriving_ui.Ui_ManualDriving.btn_home  # 수동주행 - HOME
+        self.btn_up = manualDriving_ui.Ui_ManualDriving.btn_up  # 수동주행 - 직진
+        self.btn_left = manualDriving_ui.Ui_ManualDriving.btn_left  # 수동주행 - 좌회전
+        self.btn_down = manualDriving_ui.Ui_ManualDriving.btn_down  # 수동주행 - 후진
+        self.btn_right = manualDriving_ui.Ui_ManualDriving.btn_right  # 수동주행 - 우회전
+        self.btn_stop = manualDriving_ui.Ui_ManualDriving.btn_stop
+        self.btn_emrCall_manualDriving = manualDriving_ui.Ui_ManualDriving.btn_emrCall  # 수동주행 - 긴급호출
+        # emrCall_ui ------
+        self.btn_home_emrCall = emrCall_ui.Ui_EmrCall.btn_home
+
+        # 버튼 이벤트 ------------------------------------------------------------
+        # mainPage_ui ------
+        self.btn_selDes.clicked.connect(self.show_selDestination)
+        self.btn_manDriving.clicked.connect(self.show_manualDriving)
+        self.btn_emrCall.clicked.connect(self.show_emrCall)
+        # selDestination_ui ------
+        self.btn_home_selDestination.clicked.connect(self.show_mainPage)
+        self.btn1_1f.clicked.connect(partial(self.call_assign_des, btn=self.btn1_1f, btn_list=self.btn_list))
+        self.btn2_1f.clicked.connect(partial(self.call_assign_des, btn=self.btn2_1f, btn_list=self.btn_list))
+        self.btn3_1f.clicked.connect(partial(self.call_assign_des, btn=self.btn3_1f, btn_list=self.btn_list))
+        self.btn4_1f.clicked.connect(partial(self.call_assign_des, btn=self.btn4_1f, btn_list=self.btn_list))
+        self.btn5_1f.clicked.connect(partial(self.call_assign_des, btn=self.btn5_1f, btn_list=self.btn_list))
+        self.btn6_1f.clicked.connect(partial(self.call_assign_des, btn=self.btn6_1f, btn_list=self.btn_list))
+        self.btn7_1f.clicked.connect(partial(self.call_assign_des, btn=self.btn7_1f, btn_list=self.btn_list))
+        self.btn8_1f.clicked.connect(partial(self.call_assign_des, btn=self.btn8_1f, btn_list=self.btn_list))
+        self.btn_start.clicked.connect(lambda: self.call_start_driving(self.btn_start))
+        self.btn_emrCall_selDestination.clicked.connect(self.show_emrCall)
+        # manualDriving_ui ------
+        self.btn_home_manualDriving.clicked.connect(self.show_mainPage)
+        self.btn_up.clicked.connect(manualDriving.move_straight)
+        self.btn_left.clicked.connect(manualDriving.turn_left)
+        self.btn_down.clicked.connect(manualDriving.move_back)
+        self.btn_right.clicked.connect(manualDriving.turn_right)
+        self.btn_stop.clicked.connect(manualDriving.stop_run)
+        self.btn_emrCall_manualDriving.clicked.connect(self.show_emrCall)
+        # emrCall_ui ------
+        self.btn_home_emrCall.clicked.connect(self.show_mainPage)
+
+    def show_mainPage(self):
+        # selDestination 페이지로 전환
+        print("HOME")
+        # 인덱스 전환
+        self.stacked_widget.setCurrentIndex(0)
+        self.current_page_index = 0
+
+    def show_selDestination(self):
+        # selDestination 페이지로 전환
+        print("목적지 설정")
+        # 인덱스 전환
+        self.stacked_widget.setCurrentIndex(1)
+        self.current_page_index = 1
+
+    def show_manualDriving(self):
+        # manualDriving 페이지로 전환
+        print("수동주행")
+        # 인덱스 전환
+        self.stacked_widget.setCurrentIndex(2)
+        self.current_page_index = 2
+
+    def show_emrCall(self):
+        # emrCall 페이지로 전환
+        print("긴급호출")
+        # 인덱스 전환
+        self.stacked_widget.setCurrentIndex(3)
+        self.current_page_index = 3
+
+        if self.current_page_index == 3:
+            text = "위급상황입니다."
+
+            # QTimer를 이용한 소리 지연
+            def delayed_sound():
+                emrCall.text_to_speech(text)
+
+            # 1000 밀리초 (1초) 후에 delayed_sound 함수 호출
+            QTimer.singleShot(500, delayed_sound)
+
+    def call_assign_des(self, btn, btn_list):
+        print("call_assign_des")
+        assign_des(btn, btn_list)
+
+    def call_start_driving(self, btn):
+        print("call_start_driving")
+        start_driving(btn)
+
+
+if __name__ == "__main__":
+    # QApplication : 프로그램을 실행시켜주는 클래스
+    app = QApplication(sys.argv)
+
+    # WindowClass의 인스턴스 생성
+    myWindow = WindowClass()
+
+    # 프로그램 화면을 보여주는 코드
+    myWindow.show()
+
+    # 프로그램을 이벤트루프로 진입시키는(프로그램을 작동시키는) 코드
+    app.exec_()
---
+++ selDestination.py
@@ -0,0 +1,72 @@
+import selDestination_ui
+
+des_x, des_y = 0, 0
+
+
+def assign_des(btn, btn_list):
+    print(btn.text())
+    global des_x, des_y  # 외부에서 전역 변수를 사용하기 위해 global 선언
+
+    # 모든 버튼의 styleSheet를 초기화하는 코드 필요
+    for other_btn_name in btn_list:
+        other_btn = getattr(selDestination_ui.Ui_selDestination, other_btn_name)
+        other_btn.setStyleSheet("""
+                background-color: #cfe3ac;
+                text-align: left;
+                padding: 5px;
+                padding-left: 20px;
+                border: none;
+            """)
+
+    # 클릭한 버튼의 배경색상 변경
+    btn.setStyleSheet("""
+                        background-color: #a1c464;
+                        text-align: left;
+                        padding: 5px;
+                        padding-left: 20px;
+                        border: none;
+    """)
+
+    # db에서 조회한 좌표를 변수에 할당
+    if btn.text() == "CT촬영실":
+        des_x = 1
+        des_y = 1
+    elif btn.text() == "비뇨기과":
+        des_x = 1
+        des_y = 2
+    elif btn.text() == "비상계단":
+        des_x = 1
+        des_y = 3
+    elif btn.text() == "이비인후과":
+        des_x = 2
+        des_y = 1
+    elif btn.text() == "접수처":
+        des_x = 2
+        des_y = 2
+    elif btn.text() == "치과":
+        des_x = 2
+        des_y = 3
+    elif btn.text() == "화장실":
+        des_x = 3
+        des_y = 1
+    elif btn.text() == "핵의학과":
+        des_x = 3
+        des_y = 2
+    print(des_x, des_y)
+
+
+def start_driving(btn):
+    global des_x, des_y
+    print(des_x, des_y)
+    # 클릭 시 버튼 텍스트 전환
+    if btn.text() == "주행시작":
+        btn.setText("정지")
+        # 탭 비활성화
+        # selDestination_ui.tabs.setDisabled(True)
+    elif btn.text() == "정지":
+        btn.setText("주행시작")
+        # 탭 활성화
+        # selDestination_ui.tabs.setEnabled(True)
+    else:
+        btn.setText("주행시작")
+    # 지정좌표로 이동할 수 있는 py파일에 연결
