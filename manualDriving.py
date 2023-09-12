db50988f98fcd2e191cff77b9f8bdea5078c2cb4
CSJin <hamony1029@naver.com>
2023-09-12 21:14:10 +0900

    수동주행 publishing 적용v.1.0.0

--- manualDriving.py
+++ manualDriving.py
@@ -1,36 +1,58 @@
 import os
+import rospy
+from std_msgs.msg import String
+import time
 
 # cmd = "python ~/app/talker.py "
 cmd = "python ./test.py "
 
+
+# 노드 초기화
+rospy.init_node('manualDriving_publisher', anonymous=True)
+
+ # 'hello' 토픽으로 메시지를 발행할 Publisher 생성
+pub = rospy.Publisher('direction', String, queue_size=1)
+rate = rospy.Rate(1)
+msg = String()
+
 def move_straight(self):
-    # talker.py 파일 연결
-    cmd_direction = cmd + "w"
-    print(cmd_direction)
-    os.system(cmd_direction)
+    # publishing
+    msg = 'w'
+    
+    while not rospy.is_shutdown():
+        pub.publish(msg)
+        rate.sleep()
 
 def turn_left(self):
-    # talker.py 파일 연결
-    cmd_direction = cmd + "a"
-    print(cmd_direction)
-    os.system(cmd_direction)
+    # publishing
+    msg = 'a'
+    
+    while not rospy.is_shutdown():
+        pub.publish(msg)
+        rate.sleep()
 
 def move_back(self):
-    # talker.py 파일 연결
-    cmd_direction = cmd + "x"
-    print(cmd_direction)
-    os.system(cmd_direction)
+    # publishing
+    msg = 'x'
+    
+    while not rospy.is_shutdown():
+        pub.publish(msg)
+        rate.sleep()
 
 def turn_right(self):
-    # talker.py 파일 연결
-    cmd_direction = cmd + "d"
-    print(cmd_direction)
-    os.system(cmd_direction)
+    # publishing
+    msg = 'd'
+    
+    while not rospy.is_shutdown():
+        pub.publish(msg)
+        rate.sleep()
 
 def stop_run(self):
-    # talker.py 파일 연결
-    cmd_direction = cmd + "s"
-    print(cmd_direction)
-    os.system(cmd_direction)
+    # publishing
+    msg = 's'
+    
+    while not rospy.is_shutdown():
+        pub.publish(msg)
+        rate.sleep()
 
 
