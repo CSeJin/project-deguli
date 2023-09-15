dc53c229492f7e0041b7fdcc1b62a2dd8474faab
CSJin <hamony1029@naver.com>
2023-09-15 14:29:51 +0900

    수동주행 v.2.1.0

--- remote_pc/manualDriving_to_openCR.py
+++ remote_pc/manualDriving_to_openCR.py
@@ -1,10 +1,11 @@
 import rospy
 from geometry_msgs.msg import Twist
 import sys, select, os
+
 if os.name == 'nt':
-  import msvcrt, time
+    import msvcrt, time
 else:
-  import tty, termios
+    import tty, termios
 
 BURGER_MAX_LIN_VEL = 0.22
 BURGER_MAX_ANG_VEL = 2.84
@@ -15,16 +16,16 @@ WAFFLE_MAX_ANG_VEL = 1.82
 LIN_VEL_STEP_SIZE = 0.01
 ANG_VEL_STEP_SIZE = 0.1
 
-
 e = """
 Communications Failed
 """
+
 
 def getKey():
     if os.name == 'nt':
         timeout = 0.1
         startTime = time.time()
-        while(1):
+        while (1):
             if msvcrt.kbhit():
                 if sys.version_info[0] >= 3:
                     return msvcrt.getch().decode()
@@ -32,59 +33,65 @@ def getKey():
                     return msvcrt.getch()
             elif time.time() - startTime > timeout:
                 return ''
-
+    
     tty.setraw(sys.stdin.fileno())
     rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
     if rlist:
         key = sys.stdin.read(1)
     else:
         key = ''
-
+    
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     return key
 
+
 def vels(target_linear_vel, target_angular_vel):
-    return "currently:\tlinear vel %s\t angular vel %s " % (target_linear_vel,target_angular_vel)
+    return "currently:\tlinear vel %s\t angular vel %s " % (target_linear_vel, target_angular_vel)
+
 
 def makeSimpleProfile(output, input, slop):
     if input > output:
-        output = min( input, output + slop )
+        output = min(input, output + slop)
     elif input < output:
-        output = max( input, output - slop )
+        output = max(input, output - slop)
     else:
         output = input
-
+    
     return output
+
 
 def constrain(input, low, high):
     if input < low:
-      input = low
+        input = low
     elif input > high:
-      input = high
+        input = high
     else:
-      input = input
-
+        input = input
+    
     return input
+
 
 def checkLinearLimitVelocity(vel):
     if turtlebot3_model == "burger":
-      vel = constrain(vel, -BURGER_MAX_LIN_VEL, BURGER_MAX_LIN_VEL)
+        vel = constrain(vel, -BURGER_MAX_LIN_VEL, BURGER_MAX_LIN_VEL)
     elif turtlebot3_model == "waffle" or turtlebot3_model == "waffle_pi":
-      vel = constrain(vel, -WAFFLE_MAX_LIN_VEL, WAFFLE_MAX_LIN_VEL)
+        vel = constrain(vel, -WAFFLE_MAX_LIN_VEL, WAFFLE_MAX_LIN_VEL)
     else:
-      vel = constrain(vel, -BURGER_MAX_LIN_VEL, BURGER_MAX_LIN_VEL)
-
+        vel = constrain(vel, -BURGER_MAX_LIN_VEL, BURGER_MAX_LIN_VEL)
+    
     return vel
+
 
 def checkAngularLimitVelocity(vel):
     if turtlebot3_model == "burger":
-      vel = constrain(vel, -BURGER_MAX_ANG_VEL, BURGER_MAX_ANG_VEL)
+        vel = constrain(vel, -BURGER_MAX_ANG_VEL, BURGER_MAX_ANG_VEL)
     elif turtlebot3_model == "waffle" or turtlebot3_model == "waffle_pi":
-      vel = constrain(vel, -WAFFLE_MAX_ANG_VEL, WAFFLE_MAX_ANG_VEL)
+        vel = constrain(vel, -WAFFLE_MAX_ANG_VEL, WAFFLE_MAX_ANG_VEL)
     else:
-      vel = constrain(vel, -BURGER_MAX_ANG_VEL, BURGER_MAX_ANG_VEL)
-
+        vel = constrain(vel, -BURGER_MAX_ANG_VEL, BURGER_MAX_ANG_VEL)
+    
     return vel
+
 
 if __name__ == "__main__":
     if __name__ == "__main__":
@@ -98,8 +105,17 @@ if __name__ == "__main__":
         
         try:
             while not rospy.is_shutdown():
-                linear_vel = 1.2
-                angular_vel = 0.5
+                if sys.argv[2] == 'w':
+                    linear_vel = 1.2
+                elif sys.argv[2] == 'x':
+                    linear_vel = -1.2
+                elif sys.argv[2] == 'd':
+                    angular_vel = 0.5
+                elif sys.argv[2] == 'a':
+                    angular_vel = -0.5
+                elif sys.argv[2] == 's':
+                    linear_vel = 0
+                    angular_vel = 0
                 
                 twist = Twist()
                 twist.linear.x = linear_vel
