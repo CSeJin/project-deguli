007558bd892b579210a99f5c494a419d6901071d
CSJin <hamony1029@naver.com>
2023-09-18 21:48:47 +0900

    direction-sub와 pub-to-openCR 병합 시도.v1

--- remote_pc/manualDriving_to_openCR.py
+++ remote_pc/manualDriving_to_openCR.py
@@ -1,5 +1,6 @@
 import rospy
 from geometry_msgs.msg import Twist
+from std_msgs.msg import String
 import sys, select, os
 
 if os.name == 'nt':
@@ -35,52 +36,67 @@ def getKey():
     else:
         key = ''
 
-
-if __name__ == "__main__":
-    if os.name != 'nt':
-        settings = termios.tcgetattr(sys.stdin)
-    
-    # to_openCR: openCR로 주행 명령 publishing
-    rospy.init_node('turtlebot3_teleop')
-    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-    
-    turtlebot3_model = rospy.get_param("model", "burger")
-    
+def direction_callback(msg):
     try:
-        while not rospy.is_shutdown():
-            key = getKey()
-            
-            if sys.argv[1] == 'w':
-                linear_vel = 0.15
-                angular_vel = 0
-            elif sys.argv[1] == 'x':
-                linear_vel = -0.15
-                angular_vel = 0
-            elif sys.argv[1] == 'd':
-                linear_vel = 0
-                angular_vel = 0.06
-            elif sys.argv[1] == 'a':
-                linear_vel = 0
-                angular_vel = -0.06
-            elif sys.argv[1] == 's':
-                linear_vel = 0
-                angular_vel = 0
-            elif key == '\x03':
-                break
-            
-            twist = Twist()
-            twist.linear.x = linear_vel
-            twist.angular.z = angular_vel
-            pub.publish(twist)
-    
+        if msg.data == 'w':
+            linear_vel = 0.15
+            angular_vel = 0
+            print(msg.data)
+        elif msg.data == 'x':
+            linear_vel = -0.15
+            angular_vel = 0
+            print(msg.data)
+        elif msg.data == 'd':
+            linear_vel = 0
+            angular_vel = 0.06
+            print(msg.data)
+        elif msg.data == 'a':
+            linear_vel = 0
+            angular_vel = -0.06
+            print(msg.data)
+        elif msg.data == 's':
+            linear_vel = 0
+            angular_vel = 0
+            print(msg.data)
+        else:
+            print(e)
+        
+        twist = Twist()
+        twist.linear.x = linear_vel
+        twist.angular.z = angular_vel
+        pub.publish(twist)
+        
     except KeyboardInterrupt:
-        print(e)
+        print(e+": pub to openCR")
     
     finally:
         twist = Twist()
         twist.linear.x = 0.0
         twist.angular.z = 0.0
         pub.publish(twist)
+
+
+if __name__ == "__main__":
+    if os.name != 'nt':
+        settings = termios.tcgetattr(sys.stdin)
+    
+    # from_raspberryPi: raspberryPi로부터 이동 명령 subscribe
+    rospy.init_node('manualDriving_subscriber', anonymous=True)
+    rate = rospy.Rate(1)
+    
+    # to_openCR: openCR로 주행 명령 publishing
+    rospy.init_node('turtlebot3_teleop')
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+    turtlebot3_model = rospy.get_param("model", "burger")
+    
+    try:
+        while not rospy.is_shutdown():
+            # direction 토픽 subscribe
+            sub = rospy.Subscriber('/direction', String, direction_callback, queue_size=1)
+            rate.sleep()
+
+    except KeyboardInterrupt:
+        print(e)
     
     if os.name != 'nt':
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
