a39a1abc5628609b26895f965b369ed9681784b3
CSJin <hamony1029@naver.com>
2023-09-19 04:37:16 +0900

    병합 시도.v4: 명령 입력 signal 추가

--- remote_pc/manualDriving_to_openCR.py
+++ remote_pc/manualDriving_to_openCR.py
@@ -15,63 +15,69 @@ e = """
 Communications Failed
 """
 
+# 플래그 초기화
+command_received = False
+linear_vel = 0
+angular_vel = 0
+
 
 def direction_callback(msg):
-    linear_vel = 0
-    angular_vel = 0
-
-    rate = rospy.Rate(1)
-    
+    global command_received, linear_vel, angular_vel
     try:
-        while not rospy.is_shutdown():  # 무한 루프 시작
-            if msg.data == 'w':
-                linear_vel = 0.10
-                angular_vel = 0
-                print(msg.data+", linear_vel: "+str(linear_vel))
-            elif msg.data == 'x':
-                linear_vel = -0.10
-                angular_vel = 0
-                print(msg.data+", linear_vel: "+str(linear_vel))
-            elif msg.data == 'd':
-                linear_vel = 0
-                angular_vel = 0.10
-                print(msg.data+", angular_vel: "+str(angular_vel))
-            elif msg.data == 'a':
-                linear_vel = 0
-                angular_vel = -0.10
-                print(msg.data+", angular_vel: "+str(angular_vel))
-            elif msg.data == 's':
-                linear_vel = 0
-                angular_vel = 0
-                print(msg.data)
-            else:
-                print(e)
-            
-            # to_openCR: openCR로 주행 명령 publishing
-            pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-    
-            twist = Twist()
-            twist.linear.x = linear_vel
-            twist.angular.z = angular_vel
-            pub.publish(twist)
-            
-            rate.sleep()
+        if msg.data == 'w':
+            linear_vel = 0.07
+            angular_vel = 0
+            command_received = True
+            print(msg.data + ", linear_vel: " + str(linear_vel))
+        elif msg.data == 'x':
+            linear_vel = -0.07
+            angular_vel = 0
+            command_received = True
+            print(msg.data + ", linear_vel: " + str(linear_vel))
+        elif msg.data == 'd':
+            linear_vel = 0
+            angular_vel = 0.10
+            command_received = True
+            print(msg.data + ", angular_vel: " + str(angular_vel))
+        elif msg.data == 'a':
+            linear_vel = 0
+            angular_vel = -0.10
+            command_received = True
+            print(msg.data + ", angular_vel: " + str(angular_vel))
+        elif msg.data == 's':
+            linear_vel = 0
+            angular_vel = 0
+            command_received = True
+            print(msg.data)
+        else:
+            print(e)
     
     except KeyboardInterrupt:
         print(e + ": pub to openCR")
-        
+
 
 if __name__ == "__main__":
     if os.name != 'nt':
         settings = termios.tcgetattr(sys.stdin)
     
-    # from_raspberryPi: raspberryPi로부터 이동 명령 subscribe
     rospy.init_node('turtlebot3_teleop', anonymous=True)
+    rate = rospy.Rate(10)
     
     try:
         while not rospy.is_shutdown():
-            # direction 토픽 subscribe
-            sub = rospy.Subscriber('/direction', String, direction_callback, queue_size=1)
+            if command_received:
+                # to_openCR: openCR로 주행 명령 publishing
+                pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+                
+                twist = Twist()
+                twist.linear.x = linear_vel
+                twist.angular.z = angular_vel
+                pub.publish(twist)
+                
+                # 플래그 초기화
+                command_received = False
+            
+            rate.sleep()
     
     except KeyboardInterrupt:
         print(e)
