ce3fed5817eb826d25b86dfe2e029e0f4e06c7e3
CSJin <hamony1029@naver.com>
2023-10-12 19:43:23 +0900

    수정

--- remote_pc/manualDriving_to_openCR.py
+++ remote_pc/manualDriving_to_openCR.py
@@ -8,46 +8,25 @@ rospy.init_node('turtlebot_controller', anonymous=True)
 
 cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 move_cmd = Twist()
-current_command = ''  # 현재 명령을 저장할 변수
 
 
-def msg_callback(msg):
-    global current_command
-    
-    if msg.data == 'w':
-        # 전진 명령
-        current_command = 'w'
-    elif msg.data == 'a':
-        # 좌회전 명령
-        current_command = 'a'
-    elif msg.data == 'd':
-        # 우회전 명령
-        current_command = 'd'
-    elif msg.data == 'x':
-        # 후진 명령
-        current_command = 'x'
-    elif msg.data == 's':
-        # 정지 명령
-        current_command = 's'
-
-
-def control_robot():
+def control_robot(msg):
     rate = rospy.Rate(10)  # 10Hz로 루프 실행
     
     while not rospy.is_shutdown():
-        if current_command == 'w':
+        if msg.data == 'w':
             # 전진
             move_cmd.linear.x = 0.05
             move_cmd.angular.z = 0.0
-        elif current_command == 'a':
+        elif msg.data == 'a':
             # 좌회전
             move_cmd.linear.x = 0.0
             move_cmd.angular.z = 0.3
-        elif current_command == 'd':
+        elif msg.data == 'd':
             # 우회전
             move_cmd.linear.x = 0.0
             move_cmd.angular.z = -0.3
-        elif current_command == 'x':
+        elif msg.data == 'x':
             # 후진
             move_cmd.linear.x = -0.05
             move_cmd.angular.z = 0.0
@@ -62,7 +41,7 @@ def control_robot():
 
 if __name__ == '__main__':
     try:
-        rospy.Subscriber('/direction', String, msg_callback, queue_size=1)
-        control_robot()
+        rospy.Subscriber('/direction', String, control_robot, queue_size=1)
+        rospy.spin()
     except rospy.ROSInterruptException:
         pass
